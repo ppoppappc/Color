c3e3a7b3ecf928b8b5c8f585340c7ea147ffcc70
ppoppappc <ppoppappc@gmail.com>
2019-05-02 13:09:04 +0800

    changecolor

--- app/src/main/java/cs2c/ppoppappc/color/MainActivity.java
+++ app/src/main/java/cs2c/ppoppappc/color/MainActivity.java
@@ -1,8 +1,13 @@
 package cs2c.ppoppappc.color;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
 import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,5 +24,27 @@ public class MainActivity extends AppCompatActivity {
         txvB = findViewById(R.id.textView4);
         txv = findViewById(R.id.textView5);
 
+    }
+
+    public void changeColor(View v){
+        Random X = new Random();
+
+        int red = X.nextInt(256);//0~255
+        txvR.setText("Red:"+red);
+        txvR.setTextColor(Color.rgb(red,0,0));
+
+        int green = X.nextInt(256);//0~255
+        txvG.setText("Green:"+green);
+        txvG.setTextColor(Color.rgb(0,green,0));
+
+        int blue = X.nextInt(256);//0~255
+        txvB.setText("Blue:"+blue);
+        txvB.setTextColor(Color.rgb(0,0,blue));
+
+        text += red + "," + green + "," + blue+"\n";
+        txv.setTextSize(16);
+        txv.setText(text);
+        txv.setMovementMethod(new ScrollingMovementMethod());
+        txv.setBackgroundColor(Color.rgb(red,green,blue));
     }
 }
